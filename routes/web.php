1334a30e1b73a176326e5e4ea0933d79065f354d
wil-asus <elsalvadorazul@outlook.es>
2023-10-16 22:07:35 -0600

    cap8 listo voy para cap9

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,25 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', 'welcome')->name('welcome');
+Route::view('/','welcome')->name('welcome');
 
+
+
+
+
+Route::middleware('auth')->group(function () {
+
+Route::view('/dashboard', 'dashboard')->name('dashboard');
+
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
 Route::get('/chirps', function () {
-    return 'esta es lagina de chirps';
+    return view('chirps.index');
 })->name('chirps.index');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
