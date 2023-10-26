f3677b10786428da2d93edc8dd1bc6c656c649f0
wil-asus <elsalvadorazul@outlook.es>
2023-10-26 13:51:31 -0600

    saliendo del 13 y voy para el 14

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -12,7 +12,7 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        //
+            return view('chirps.index');
     }
 
     /**
@@ -28,7 +28,16 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+    //insert into data base
+    Chirp::create([
+        'message' => $request->get('message'),
+        'user_id' => auth()->id(),
+    ]);
+
+    return to_route('chirps.index')->with('status','Chirp creado sactifactoriamente');
+
+
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Chirp;
 
 
 Route::view('/','welcome')->name('welcome');
@@ -19,27 +19,11 @@ Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit')
 Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::get('/chirps', function () {
-    return view('chirps.index');
-})->name('chirps.index');
+Route::get('/chirps',[ChirpController::class, 'index'] )
+   ->name('chirps.index');
 
-Route::post('/chirps', function () {
-
-    //insert into data base
-    Chirp::create([
-        'message' => request('message'),
-        'user_id' => auth()->id(),
-    ]);
-
-    return to_route('chirps.index')->with('status','Chirp creado sactifactoriamente');
-    // return redirect()->route('chirps.index');
-
-});
-
-
-
-
-
+Route::post('/chirps', [ChirpController::class, 'store'])
+->name('chirps.store');
 });
 
 require __DIR__.'/auth.php';
